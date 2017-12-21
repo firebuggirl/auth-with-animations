7debcae58f4294572360e0c7f1f7c67de84a3e2b
Juliette <julietteredheadgirl@yahoo.com>
2017-12-21 15:57:14 -0600

    test home url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { UserComponent } from "./user/user.component";
 import { LoginComponent } from "./login/login.component";
 
 export const ROUTES: Routes = [
-  // { path: '', component: HomeComponent },
+   { path: '', component: HomeComponent },
   // { path: 'callback', component: CallbackComponent },
   // { path: '**', redirectTo: '' }
 
-  { path: '',
-  redirectTo: 'home',
-   pathMatch: 'full' },
+  // { path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+
   {
     path: 'home',
     component: HomeComponent,
