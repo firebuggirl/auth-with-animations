b9f40972e842b2c9917ab3dd14753d4ccb4248de
Juliette <julietteredheadgirl@yahoo.com>
2017-12-22 22:45:42 -0600

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,10 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+//require('dotenv').config({ path: '../.env' });
 
 export const environment = {
   production: false,
-  clientID: 'nAG2jFnP2fqoF6lWtNxIVSYu2tEGC4vW',
-  domain: 'juliettet.auth0.com'
+  clientID: process.env.AUTH_CLIENT_ID,
+  domain: process.env.AUTH_DOMAIN
 };
