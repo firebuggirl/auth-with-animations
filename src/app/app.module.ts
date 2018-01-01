518c2cabbc9a4521f32f1edea53a7b935a7c14f9
Juliette <julietteredheadgirl@yahoo.com>
2017-12-31 19:09:34 -0600

    create new user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CallbackComponent } from './callback/callback.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guard/auth.guard';
 import { UserComponent } from './user/user.component';
+
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
