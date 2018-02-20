f40eeb36d585e1f4d33ac59ff817a108f0cb11d5
Juliette <julietteredheadgirl@yahoo.com>
2018-02-19 20:54:28 -0600

    put routes back in server.js...see if Heroku build succeeds

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const mongodb = require("mongodb");
-const ObjectID = mongodb.ObjectID;
-
-mongoose.connect(process.env.LOCAL_DB || process.env.DATABASE);
-mongoose.set('debug', true);
-mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-});
-
-var db = mongoose.connection;
-// mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
+// const mongodb = require("mongodb");
+// const ObjectID = mongodb.ObjectID;
+//
+// mongoose.connect(process.env.LOCAL_DB || process.env.DATABASE);
+// mongoose.set('debug', true);
+// mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+// mongoose.connection.on('error', (err) => {
+//   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+// });
+//
+// var db = mongoose.connection;
+// // mongo error
+// db.on('error', console.error.bind(console, 'connection error:'));
 
 const Contact = require('../models/contacts');
 
@@ -63,121 +63,121 @@ function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
-router.get('/api/public', function(req, res) {
-  res.json({
-    message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-  });
-});
-
-router.get('/api/private', checkJwt, checkScopes, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-  });
-});
+// router.get('/api/public', function(req, res) {
+//   res.json({
+//     message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+//   });
+// });
+//
+// router.get('/api/private', checkJwt, checkScopes, function(req, res) {
+//   res.json({
+//     message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+//   });
+// });
 // CONTACTS API ROUTES BELOW
 
-router.get("/api/contacts", function(req, res) {
-  db.collection('contacts').find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-});
-
-router.post("/api/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
-
-  if (!req.body.name) {
-    handleError(res, "Invalid user input", "Must provide a name.", 400);
-  }
-
-  db.collection('contacts').insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
-
-/*  "/api/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-router.get("/api/contacts/:id", function(req, res) {
-  const contactid = req.params.contactid;
-  //db.collection('contacts').findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-  //db.collection('contacts').findOne({ _id: new ObjectID(req.session.contactId) }, function(err, doc) {
-  db.collection('contacts').findOne( contactid , function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contact");
-    } else {
-      res.status(200).json(doc);
-      console.log(contactid);
-    }
-  });
-});
-
-
-router.put("/api/contacts/:id", function(req, res) {
-  const name = req.body.name;
-  const email = req.body.email;
-  const address = req.body.address;
-  const city = req.body.city;
-  const state = req.body.state;
-  const zipcode = req.body.zipcode;
-
-  // delete name._id;
-  // delete email._id;
-  // delete address._id;
-
-  const updateAll =  { $set:
-      {
-        name: name,
-        email:email,
-       "phone.work": req.body.phone.work,
-       "phone.mobile": req.body.phone.mobile,
-        address: address,
-        city: city,
-        state: state,
-        zipcode: zipcode
-
-      }
-   };
-
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateAll, function(err, doc) {
-    if (err) {
-      res.send(err);
-        } else {
-
-       [name._id, email._id] = req.params.id;
-       var returnObj = [name, email, address];
-
-       res.status(200).json(returnObj);
-      }
-  });
-
-
-});
-
-router.delete("/api/contacts/:id", function(req, res) {
-  const contactid = req.params.contactid;
-  // db.collection('contacts').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-  //db.collection('contacts').deleteOne({_id: new ObjectID(req.session.contactId) }, function(err, result) {
-  db.collection('contacts').deleteOne(contactid, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete contact");
-    } else {
-      //res.status(200).json(req.params.id);
-      res.status(200).json(contactid);
-    }
-  });
-});
+// router.get("/api/contacts", function(req, res) {
+//   db.collection('contacts').find({}).toArray(function(err, docs) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get contacts.");
+//     } else {
+//       res.status(200).json(docs);
+//     }
+//   });
+// });
+//
+// router.post("/api/contacts", function(req, res) {
+//   var newContact = req.body;
+//   newContact.createDate = new Date();
+//
+//   if (!req.body.name) {
+//     handleError(res, "Invalid user input", "Must provide a name.", 400);
+//   }
+//
+//   db.collection('contacts').insertOne(newContact, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to create new contact.");
+//     } else {
+//       res.status(201).json(doc.ops[0]);
+//     }
+//   });
+// });
+//
+// /*  "/api/contacts/:id"
+//  *    GET: find contact by id
+//  *    PUT: update contact by id
+//  *    DELETE: deletes contact by id
+//  */
+//
+// router.get("/api/contacts/:id", function(req, res) {
+//   const contactid = req.params.contactid;
+//   //db.collection('contacts').findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//   //db.collection('contacts').findOne({ _id: new ObjectID(req.session.contactId) }, function(err, doc) {
+//   db.collection('contacts').findOne( contactid , function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get contact");
+//     } else {
+//       res.status(200).json(doc);
+//       console.log(contactid);
+//     }
+//   });
+// });
+//
+//
+// router.put("/api/contacts/:id", function(req, res) {
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const address = req.body.address;
+//   const city = req.body.city;
+//   const state = req.body.state;
+//   const zipcode = req.body.zipcode;
+//
+//   // delete name._id;
+//   // delete email._id;
+//   // delete address._id;
+//
+//   const updateAll =  { $set:
+//       {
+//         name: name,
+//         email:email,
+//        "phone.work": req.body.phone.work,
+//        "phone.mobile": req.body.phone.mobile,
+//         address: address,
+//         city: city,
+//         state: state,
+//         zipcode: zipcode
+//
+//       }
+//    };
+//
+//   db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateAll, function(err, doc) {
+//     if (err) {
+//       res.send(err);
+//         } else {
+//
+//        [name._id, email._id] = req.params.id;
+//        var returnObj = [name, email, address];
+//
+//        res.status(200).json(returnObj);
+//       }
+//   });
+//
+//
+// });
+//
+// router.delete("/api/contacts/:id", function(req, res) {
+//   const contactid = req.params.contactid;
+//   // db.collection('contacts').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+//   //db.collection('contacts').deleteOne({_id: new ObjectID(req.session.contactId) }, function(err, result) {
+//   db.collection('contacts').deleteOne(contactid, function(err, result) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to delete contact");
+//     } else {
+//       //res.status(200).json(req.params.id);
+//       res.status(200).json(contactid);
+//     }
+//   });
+// });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const helpers = require('./helpers');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
+const Contact = require('./models/contacts');
+var CONTACTS_COLLECTION = "contacts";
+
 const mongoose = require('mongoose');
 const mongodb = require("mongodb");
 const ObjectID = mongodb.ObjectID;
@@ -82,14 +85,125 @@ const checkJwt = jwt({
 const checkScopes = jwtAuthz(['read:messages']);
 
 
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+  });
+});
 
+app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});
 
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
 
+app.get("/api/contacts", function(req, res) {
+  db.collection('contacts').find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
 
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection('contacts').insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/api/contacts/:id", function(req, res) {
+  const contactid = req.params.contactid;
+  //db.collection('contacts').findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+  //db.collection('contacts').findOne({ _id: new ObjectID(req.session.contactId) }, function(err, doc) {
+  db.collection('contacts').findOne( contactid , function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+      console.log(contactid);
+    }
+  });
+});
+
+
+app.put("/api/contacts/:id", function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const address = req.body.address;
+  const city = req.body.city;
+  const state = req.body.state;
+  const zipcode = req.body.zipcode;
+
+  // delete name._id;
+  // delete email._id;
+  // delete address._id;
+
+  const updateAll =  { $set:
+      {
+        name: name,
+        email:email,
+       "phone.work": req.body.phone.work,
+       "phone.mobile": req.body.phone.mobile,
+        address: address,
+        city: city,
+        state: state,
+        zipcode: zipcode
+
+      }
+   };
+
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateAll, function(err, doc) {
+    if (err) {
+      res.send(err);
+        } else {
+
+       [name._id, email._id] = req.params.id;
+       var returnObj = [name, email, address];
+
+       res.status(200).json(returnObj);
+      }
+  });
+
+
+});
+
+app.delete("/api/contacts/:id", function(req, res) {
+  const contactid = req.params.contactid;
+  // db.collection('contacts').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+  //db.collection('contacts').deleteOne({_id: new ObjectID(req.session.contactId) }, function(err, result) {
+  db.collection('contacts').deleteOne(contactid, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      //res.status(200).json(req.params.id);
+      res.status(200).json(contactid);
+    }
+  });
+});
 
 
 
